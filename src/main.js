373febfd7cfafcb1100fe0e177a79586617ef76a
AchrefFast <justnevergiveup001@gmail.com>
2021-12-12 00:06:45 +0100

    Added , showing one question at a time feature, using details element instead of simple div, animation

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
 
 function main() {
-    const answers = document.querySelectorAll('.question__q');
-    answers.forEach(elm => {
-        elm.addEventListener('click', (e) => {
-            const parent = elm.parentElement;
-            const arrow = parent.firstElementChild.lastElementChild;
-            arrow.classList.toggle('flip');
-            elm.classList.toggle('bold');
-            console.log(arrow);
-            parent.lastElementChild.classList.toggle('appear');
 
-
-
-        });
+    const details = document.querySelectorAll('details');
+    details.forEach(elm => {
+        elm.addEventListener('click', () => {
+            details.forEach(question => {
+                if (question.hasAttribute('open') && elm != question) {
+                    question.removeAttribute('open');
+                }
+            })
+        })
     });
 }
 
-
-
-
 window.addEventListener('DOMContentLoaded', main);
+
+//=======================================Without details element===========================//
+
+// I used this when I build the question section without the "details" tag.
+// By using the details element I didn't need the mechanisem of opening and closing the question anymore.
+// This code will be needed to create the hide/show action of the questions.
+
+// function main() {
+//     const answers = document.querySelectorAll('.question__q');
+//     answers.forEach(elm => {
+//         elm.addEventListener('click', (e) => {
+//             const parent = elm.parentElement;
+//             const arrow = parent.firstElementChild.lastElementChild;
+//             arrow.classList.toggle('flip');
+//             elm.classList.toggle('bold');
+//             console.log(arrow);
+//             parent.lastElementChild.classList.toggle('appear');
+//         });
+//     });
+// }
+
+// window.addEventListener('DOMContentLoaded', main);
